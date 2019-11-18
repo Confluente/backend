9976e0edb9b26d810df984b23bc67a480bda03ca
rdvhoorn <robinvh@live.nl>
2019-11-18 13:54:59 +0100

    Added group creation and viewing

--- routes/group.js
+++ routes/group.js
@@ -7,11 +7,58 @@ var permissions = require("../permissions");
 var router = express.Router();
 
 router.route("/")
-    .all(permissions.requireAll({type: "USER_MANAGE"}))
+    .all(permissions.requireAll({type: "GROUP_MANAGE"}))
     .get(function (req, res, next) {
-        Group.findAll().then(function (results) {
+        Group.findAll({
+            attributes: ["id", "fullName", "displayName", "description", "email", "canOrganize"],
+            order: [
+                ["id", "ASC"]
+            ]
+        }).then(function (results) {
             res.send(results);
         });
+    })
+    .post(function (req, res, next) {
+        if (!res.locals.session) {
+            return res.sendStatus(401);
+        }
+        if (!req.body.displayName || !req.body.fullName || !req.body.description || !req.body.email) {
+            return res.sendStatus(400);
+        }
+
+        permissions.check(res.locals.session.user, {
+            type: "GROUP_CREATE",
+            value: req.body.organizer
+        }).then(function (result) {
+            if (!result) return res.sendStatus(403);
+            return Group.create(req.body).then(function (result) {
+                res.status(201).send(result);
+            });
+        }).done();
     });
 
+router.route("/:id")
+    .all(function (req, res, next) {
+        var id = req.params.id;
+        Group.findByPk(req.params.id, {
+            attributes: ["id", "fullName", "displayName", "description", "email", "canOrganize"]
+        }).then(function (group) {
+            if (group === null) {
+                res.status(404).send({status: "Not Found"});
+            } else {
+                res.locals.group = group;
+                next();
+            }
+        });
+    })
+    .get(function (req, res) {
+        var user = res.locals.session ? res.locals.session.user : null;
+        permissions.check(user, {type: "GROUP_VIEW", value: req.params.id}).then(function (result) {
+            if (!result) {
+                return res.sendStatus(403);
+            }
+            var group = res.locals.group;
+            res.send(group);
+        });
+    });
 module.exports = router;
