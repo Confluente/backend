649de61b3bffa92e6f2ebe38dd3557203f6cd193
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2020-05-23 17:04:55 +0200

    Added comments for activity model

--- models/activity.js
+++ models/activity.js
@@ -6,37 +6,98 @@ var User = require("./user");
 var Group = require("./group");
 
 var Activity = sequelize.define('activity', {
+    /*
+     * Name of the activity
+     */
     name: Sequelize.STRING,
+
+    /*
+     * Description of the activity
+     */
     description: Sequelize.STRING,
+
+    /*
+     * Location of the activity
+     */
     location: Sequelize.STRING,
+
+    /*
+     * Date of the activity
+     */
     date: Sequelize.DATE,
+
+    /*
+     * Start time of the activity
+     */
     startTime: Sequelize.TIME,
+
+    /*
+     * End time of the activity
+     */
     endTime: Sequelize.TIME,
+
+    /*
+     * canSubscribes stores whether members can subscribe to the activity
+     */
     canSubscribe: {
         type: Sequelize.BOOLEAN,
         defaultValue: false
     },
+
+    /*
+     * Participation fee of the activity
+     */
     participationFee: Sequelize.DECIMAL,
 
-    // all form variables
+    /*
+     * Number of questions in the subscription form
+     */
     numberOfQuestions: Sequelize.INTEGER,
+
+    /*
+     * Type of questions in the subscription form.
+     * For every question the string stores whether the question is text, multiple choice or checkboxes.
+     * Types are separated by #,# delimiter
+     */
     typeOfQuestion: Sequelize.STRING,
+
+    /*
+     * Questions of the subscription form.
+     * For every question the string stores the description (actual question).
+     * Descriptions are separated by #,# delimiter.
+     */
     questionDescriptions: Sequelize.STRING,
+
+    /*
+     * Options for multiple choice and checkbox questions in form.
+     * For every question the string stores the options.
+     * Options of different questions are separated by #,#.
+     * Options for the same question are separated by #;#.
+     */
     formOptions: Sequelize.STRING,
+
+    /*
+     * Which questions are required.
+     * For every question the string stores true or false.
+     * Separated by #,#.
+     */
     required: Sequelize.STRING,
+
+    /*
+     * Subscription deadline of the activity
+     */
     subscriptionDeadline: Sequelize.DATE,
+
+    /*
+     * Stores whether the activity is published
+     */
     published: {
         type: Sequelize.BOOLEAN,
         defaultValue: false
     }
-}, {
-    instanceMethods: {
-        isAwesome: function () {
-            return Q(true);
-        }
-    }
 });
 
+// relates activities to a group that organizes the activity
 Activity.Organizer = Activity.belongsTo(Group, {as: "Organizer"});
 
 Activity.sync();
