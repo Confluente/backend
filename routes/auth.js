229e8b83f02c6696fb295015c75ad0778b76bbac
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2020-05-24 10:39:25 +0200

    Added comments to auth route

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,17 @@ var authHelper = require("../authHelper");
 var router = express.Router();
 
 router.route("/")
+    /*
+     * Function for getting the profile of the user.
+     */
     .get(function (req, res, next) {
+
+        // Check whether the response has a session (handled by express)
         if (!res.locals.session) {
             return res.sendStatus(401);
         }
+
+        // find the user of the session in the database
         User.findByPk(res.locals.session.user, {
             attributes: ["id", "email", "displayName", "isAdmin", "consentWithPortraitRight"],
             include: [{
@@ -18,46 +25,61 @@ router.route("/")
                 attributes: ["id", "displayName", "fullname", "description", "canOrganize", "email"]
             }]
         }).then(function (user) {
+
+            // get the datavalues of the user
             var profile = user.dataValues;
-            //console.log(profile.groups);
-            /*
-            return user.getGroups();
-          }).then(function (groups) {
-            //console.log(groups[0].user_group);
-            */
+
+            // set whether the user can organize activities
             profile.canOrganize = profile.isAdmin || profile.groups.some(function (e) {
                 return e.canOrganize;
             });
 
+            // send the profile back the client
             res.send(profile);
         });
     });
 
 router.route("/login")
+    /*
+     * Function for login a user in.
+     */
     .post(function (req, res, next) {
+
+        // Check if both the email and password field were filled in
         if (!req.body.email || !req.body.password) {
             return res.sendStatus(400);
         }
+
+        // initialize variables
         var credentials = {
             email: req.body.email,
             password: req.body.password
         };
+
+        // authenticate user
         authHelper.authenticate(req.body.email, req.body.password).then(function (user) {
+
+            // check if error occurred
             if (user.error === 406) {
                 return res.status(406).send(user)
             }
 
+            // check if user account is approved
             if (user.approved === false) {
                 return res.status(406).send({error: 406, data: "User account has not yet been approved"});
             }
+
             res.locals.user = user;
+
+            // start a new session and send that session back to the client
             return authHelper.startSession(user.id, req.ip)
                 .then(function (session) {
                     res.cookie('session', session.token.toString("base64"));
                     res.send(session);
                 });
         }).catch(function (err) {
-            //throw err;
+
+            //throw err
             return res.status(500).send({error: err});
         });
     });
